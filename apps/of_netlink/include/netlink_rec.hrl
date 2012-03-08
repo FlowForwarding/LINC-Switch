d8c8556505c0a72a5677e7b6de1fe31626bbe7d4
Grzegorz Stanislawski <stangrze@yahoo.com>
2012-03-08 15:35:46 +0100

    Basic encoder/decoder for netlink protocol #8258

---
+++ apps/of_netlink/include/netlink_rec.hrl
@@ -0,0 +1,118 @@
+%%%-----------------------------------------------------------------------------
+%%% @copyright (C) 2012, Erlang Solutions Ltd.
+%%% @author Grzegorz Stanislawski <grzegorz.stanislawski@erlang-solutions.com>
+%%%-----------------------------------------------------------------------------
+
+
+-define(NLA_ALIGNTO,4).
+-define(AF_NETLINK,16).
+-define(PF_NETLINK,?AF_NETLINK).
+
+%%------------------------------------------------------------------------------
+%% Types
+%%------------------------------------------------------------------------------
+-type bit() :: 0|1 .
+%% @type bit() = 0 |1.
+%%   bit flag holder
+%% @end
+%%------------------------------------------------------------------------------
+
+
+%% struct sockaddr_nl
+-record(sockaddr_nl, {
+        family = ?AF_NETLINK :: integer(), %16b, + pad 16b
+        pid    = 0           :: integer(), %32b
+        groups = 0           ::integer()   %32b
+        }).
+
+-type sockaddr_nl() :: #sockaddr_nl{}.
+%% @type sockaddr_nl() :: #sockaddr_nl{}.
+%%   holds structure used in procket:connect.
+%%   Needs to be encoded to binary (with of_netlink:encode()) before using .
+
+%% struct nla_attr with payload
+-record(nla_attr, {
+        type :: integer(),
+        n    :: bit(),
+        o    :: bit(),
+        data :: binary()
+        }).
+
+%% struct genlmsghdr
+-record(genlmsg, {
+        cmd :: byte(),
+        version :: byte(),
+%       reserved :: binary()
+        payload :: list(#nla_attr{})
+        }).
+
+%% struct nlmsghdr
+-record(nlmsg, {
+        len   = undefined  :: integer() | undefined,
+        type    :: integer(),
+%        flags:
+        request =1 :: bit(),
+        multi   =0 :: bit(),
+        ack     =1 :: bit(),
+        echo    =0 :: bit(),
+        dumpintr=0 :: bit(),
+%this flags mean different things depending on content of payload
+%        flag0100:: bool(),
+%        flag0200:: bool(),
+%        flag0400:: bool(),
+%        flag0800:: bool(),
+        seq     :: integer(),
+        pid     :: integer(),
+        payload :: #genlmsg{}
+        }).
+
+
+%netlink types = nlmsgheader.type
+-define(NETLINK_ROUTE,           0). %  Routing/device hook
+-define(NETLINK_UNUSED,          1). %  Unused number
+-define(NETLINK_USERSOCK,        2). %  Reserved for user mode socket protocols
+-define(NETLINK_FIREWALL,        3). %  Firewalling hook
+-define(NETLINK_SOCK_DIAG,       4). %  socket monitoring
+-define(NETLINK_NFLOG,           5). %  netfilter/iptables ULOG
+-define(NETLINK_XFRM,            6). %  ipsec
+-define(NETLINK_SELINUX,         7). %  SELinux event notifications
+-define(NETLINK_ISCSI,           8). %  Open-iSCSI
+-define(NETLINK_AUDIT,           9). %  auditing
+-define(NETLINK_FIB_LOOKUP,      10).
+-define(NETLINK_CONNECTOR,       11).
+-define(NETLINK_NETFILTER,       12). %  netfilter subsystem
+-define(NETLINK_IP6_FW,          13).
+-define(NETLINK_DNRTMSG,         14). %  DECnet routing messages
+-define(NETLINK_KOBJECT_UEVENT,  15). %  Kernel messages to userspace
+-define(NETLINK_GENERIC,         16).
+-define(NETLINK_DM,              17). % not used
+-define(NETLINK_SCSITRANSPORT,   18). %  SCSI Transports
+-define(NETLINK_ECRYPTFS,        19).
+-define(NETLINK_RDMA,            20).
+-define(NETLINK_CRYPTO,          21). %  Crypto layer
+-define(NETLINK_INET_DIAG,?NETLINK_SOCK_DIAG).
+
+%% genlmsghdr commands enum
+-define(CTRL_CMD_UNSPEC, 0).
+-define(CTRL_CMD_NEWFAMILY, 1).
+-define(CTRL_CMD_DELFAMILY, 2).
+-define(CTRL_CMD_GETFAMILY, 3).
+-define(CTRL_CMD_NEWOPS, 4).
+-define(CTRL_CMD_DELOPS, 5).
+-define(CTRL_CMD_GETOPS, 6).
+-define(CTRL_CMD_NEWMCAST_GRP, 7).
+-define(CTRL_CMD_DELMCAST_GRP, 8).
+-define(CTRL_CMD_GETMCAST_GRP, 9).   % unused
+-define(CTRL_CMD_MAX, 10).   % unused
+
+%% nlattr type enum
+-define(CTRL_ATTR_UNSPEC, 0).
+-define(CTRL_ATTR_FAMILY_ID, 1).
+-define(CTRL_ATTR_FAMILY_NAME, 2).
+-define(CTRL_ATTR_VERSION, 3).
+-define(CTRL_ATTR_HDRSIZE, 4).
+-define(CTRL_ATTR_MAXATTR, 5).
+-define(CTRL_ATTR_OPS, 6).
+-define(CTRL_ATTR_MCAST_GROUPS, 7).
+-define(CTRL_ATTR_MAX, 8).
+
---
+++ apps/of_netlink/src/of_netlink.erl
@@ -0,0 +1,196 @@
+%%%-----------------------------------------------------------------------------
+%%% @copyright (C) 2012, Erlang Solutions Ltd.
+%%% @author Grzegorz Stanislawski <grzegorz.stanislawski@erlang-solutions.com>
+%%% @doc Linux Netlink interface library module.
+%%% @end
+%%%-----------------------------------------------------------------------------
+-module(of_netlink).
+-include("netlink_rec.hrl").
+%% API
+-export([encode/1,decode/1,decode_sockaddr_nl/1]).
+
+%%%===================================================================
+%%% External functions
+%%%===================================================================
+encode(Binary) when is_binary(Binary) ->
+    Binary;
+
+encode([]) ->
+    <<>>;
+
+encode(List=[#nla_attr{}|_]) ->
+    encode_nla(List,<<>>);
+
+encode(#sockaddr_nl{family=Family,pid=Pid,groups=Groups}) ->
+    <<Family:16/native-integer, 0:16/native-integer, 
+      Pid:32/native-integer,Groups:32/native-integer>>;
+
+encode(#genlmsg{cmd=Cmd,version=Ver,payload=Payload}) ->
+    Data=encode(Payload),
+    <<Cmd:8/native-integer,Ver:8/native-integer,
+      0:16/native-integer,Data/binary>>;
+
+encode(#nlmsg{len=Len,type=Type,request=R,multi=M,ack=A,echo=E,dumpintr=D,
+              seq=Seq,pid=Pid,payload=Payload}) ->
+    <<FlagsLo:8>> = <<0:3,D:1,E:1,A:1,M:1,R:1>>,
+    FlagsHi=0, % flagsHi nod done yet, it depends on payload.
+    Data=encode(Payload),
+    L= case Len of
+        undefined -> 16+size(Data);
+        LL        -> LL
+    end,
+    <<L:32/native-integer,Type:16/native-integer,
+      FlagsLo:8/native-integer,FlagsHi:8/native-integer,
+      Seq:32/native-integer,Pid:32/native-integer,Data/binary>>.
+
+decode_sockaddr_nl(<<Family:16/native-integer, _:16/native-integer,
+        Pid:32/native-integer,Groups:32/native-integer>>) ->
+    #sockaddr_nl{family=Family,pid=Pid,groups=Groups}.
+
+%netlink error 
+decode(<<L:32/native-integer,2:16/native-integer,
+         FlagsLo:1/binary,FlagsHi:8/native-integer,
+         Seq:32/native-integer,Pid:32/native-integer,Data/binary>>) ->
+    <<_:3,D:1,E:1,A:1,M:1,R:1>> = FlagsLo,
+    PayloadLen=L-16,
+    %chop trailing 0's
+    <<Data1:PayloadLen/binary,_/binary>>=Data,
+    <<ErrCode:32/native-integer,Original/binary>> = Data1,
+    Payload={error,ErrCode,decode(Original)},
+    #nlmsg{len=L,type=?NETLINK_GENERIC,request=R,multi=M,ack=A,echo=E,
+           dumpintr=D,seq=Seq,pid=Pid,payload=Payload};
+
+decode(<<L:32/native-integer,?NETLINK_GENERIC:16/native-integer,
+         FlagsLo:1/binary, FlagsHi:8/native-integer,
+         Seq:32/native-integer,Pid:32/native-integer,Data/binary>>) ->
+    <<_:3,D:1,E:1,A:1,M:1,R:1>> = FlagsLo,
+    PayloadLen=L-16,
+    %chop trailing 0's
+    <<Data1:PayloadLen/binary,_/binary>>=Data,
+    Payload=decode_genl(Data1),
+    #nlmsg{len=L,type=?NETLINK_GENERIC,request=R,multi=M,ack=A,echo=E,
+           dumpintr=D,seq=Seq,pid=Pid,payload=Payload};
+
+%default leave paload undecoded
+decode(<<L:32/native-integer,Type:16/native-integer,
+         FlagsLo:1/binary,FlagsHi:8/native-integer,
+         Seq:32/native-integer,Pid:32/native-integer,Data/binary>>) ->
+    <<_:3,D:1,E:1,A:1,M:1,R:1>> = FlagsLo,
+    PayloadLen=L-16,
+    %chop trailing 0's
+    <<Data1:PayloadLen/binary,_/binary>>=Data,
+    #nlmsg{len=L,type=Type,request=R,multi=M,ack=A,echo=E,
+           dumpintr=D,seq=Seq,pid=Pid,payload=Data1}.
+
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+nla_align(Len) ->
+    (Len + ?NLA_ALIGNTO - 1) band bnot(?NLA_ALIGNTO - 1).
+
+decode_genl(<<>>) ->
+    {error,no_genl};
+decode_genl(<<Cmd:8/native-integer,Ver:8/native-integer,
+              _:16/native-integer,Data/binary>>) ->
+    Payload=decode_nla(Data, []),
+    #genlmsg{cmd=Cmd,version=Ver,payload=Payload}.
+
+decode_nla(<<>>,NlaList) ->
+    NlaList;
+decode_nla(<<Len:16/native-integer,Rest/binary>>,NlaList) ->
+    PayloadLen=Len-4,
+    HdrPadLen=0, % should be nla_align(?NLA_HDRLEN), where -define(NLA_HDRLEN,4).
+    PayloadPadLen=nla_align(PayloadLen) -PayloadLen,
+    % Len:16/native-integer already eaten
+    <<Type:14/native-integer,O:1,N:1, _HdrPad:HdrPadLen/binary,
+      Payload:PayloadLen/binary, PayloadPad:PayloadPadLen/binary,
+      Tail/binary>> = Rest,
+    PL=PayloadLen*8,
+    Data=case Type of
+            ?CTRL_ATTR_UNSPEC      -> Payload;
+            ?CTRL_ATTR_FAMILY_ID   -> <<A:PL/native-integer>> = Payload,A;
+            ?CTRL_ATTR_FAMILY_NAME -> L=PayloadLen-1,
+                                      <<A:L/binary,0>>=Payload, 
+                                      binary_to_list(A);
+            ?CTRL_ATTR_VERSION     -> <<A:PL/native-integer>> = Payload,A;
+            ?CTRL_ATTR_HDRSIZE     -> <<A:PL/native-integer>> = Payload,A;
+            ?CTRL_ATTR_MAXATTR     -> <<A:PL/native-integer>> = Payload,A;
+            ?CTRL_ATTR_OPS        -> Payload;
+            ?CTRL_ATTR_MCAST_GROUPS-> Payload
+    end,
+    decode_nla(Tail, NlaList ++ [#nla_attr{type=Type,n=N,o=O,data=Data}]).
+
+
+encode_nla([],Acc) ->
+    Acc;
+
+encode_nla([Nla=#nla_attr{type = ?CTRL_ATTR_FAMILY_NAME,data = Data }
+           |Rest],Acc) when is_list(Data)->
+    encode_nla([Nla#nla_attr{data=list_to_binary(Data)}|Rest],Acc);
+
+encode_nla([Nla=#nla_attr{type = ?CTRL_ATTR_FAMILY_NAME,data = <<>> }
+           |Rest],Acc) ->
+    encode_nla([Nla#nla_attr{data= <<0>>}|Rest],Acc);
+
+encode_nla([#nla_attr{type = ?CTRL_ATTR_FAMILY_NAME,n=N, o=O,data = Data }
+           |Rest],Acc) when is_binary(Data)->
+    Payload=case binary:at(Data,size(Data)-1) == 0 of
+        true -> Data;
+        false -> <<Data/binary,0>>
+    end,
+    PayloadLen=size(Payload),
+    %should be nla_align(?NLA_HDRLEN), where -define(NLA_HDRLEN,4).
+    HdrPadLen = 0,
+    PayloadPadLen=nla_align(PayloadLen)-PayloadLen,
+    Len= 4 + HdrPadLen + PayloadLen,
+    NlaBin= <<Len:16/native-integer,
+              ?CTRL_ATTR_FAMILY_NAME:14/native-integer,O:1,N:1 ,0:(HdrPadLen*8),
+              Payload/binary, 0:(PayloadPadLen*8)>>,
+    encode_nla(Rest,<<Acc/binary,NlaBin/binary>>);
+
+
+encode_nla([#nla_attr{type = ?CTRL_ATTR_FAMILY_ID,n=N, o=O,data = Data }
+           |Rest],Acc) ->
+    %should be nla_align(?NLA_HDRLEN), where -define(NLA_HDRLEN,4).
+    HdrPadLen = 0,
+    PayloadLen=2, % 16bit integer
+    PayloadPadLen=nla_align(PayloadLen)-PayloadLen,
+    Len= 4 + HdrPadLen + PayloadLen,
+    NlaBin= <<Len:16/native-integer,
+              ?CTRL_ATTR_FAMILY_ID:14/native-integer,O:1,N:1, 0:(HdrPadLen*8),
+              Data:(PayloadLen*8)/native-integer, 0:(PayloadPadLen*8)>>,
+    encode_nla(Rest,<<Acc/binary,NlaBin/binary>>);
+
+
+encode_nla([#nla_attr{type = Type,n=N, o=O,data = Data }
+           |Rest],Acc) 
+        when Type==?CTRL_ATTR_VERSION; Type==?CTRL_ATTR_HDRSIZE;
+             Type==?CTRL_ATTR_MAXATTR ->
+    %should be nla_align(?NLA_HDRLEN), where -define(NLA_HDRLEN,4).
+    HdrPadLen = 0,
+    PayloadLen=4, % 64bit integer
+    PayloadPadLen=nla_align(PayloadLen)-PayloadLen,
+    Len= 4 + HdrPadLen + PayloadLen,
+    NlaBin= <<Len:16/native-integer,
+              Type:14/native-integer,O:1,N:1, 0:(HdrPadLen*8),
+              Data:(PayloadLen*8)/native-integer, 0:(PayloadPadLen*8)>>,
+    encode_nla(Rest,<<Acc/binary,NlaBin/binary>>);
+
+encode_nla([#nla_attr{type = Type,n=N, o=O,data = Data }
+           |Rest],Acc) 
+        when Type==?CTRL_ATTR_OPS; Type==?CTRL_ATTR_MCAST_GROUPS;
+             Type==?CTRL_ATTR_UNSPEC ->
+    %should be nla_align(?NLA_HDRLEN), where -define(NLA_HDRLEN,4).
+    HdrPadLen = 0,
+    PayloadLen=size(Data),
+    PayloadPadLen=nla_align(PayloadLen)-PayloadLen,
+    Len= 4 + HdrPadLen + PayloadLen,
+    NlaBin= <<Len:16/native-integer,
+              Type:14/native-integer,O:1,N:1, 0:(HdrPadLen*8),
+              Data/binary, 0:(PayloadPadLen*8)>>,
+    encode_nla(Rest,<<Acc/binary,NlaBin/binary>>).
+
+
+
+

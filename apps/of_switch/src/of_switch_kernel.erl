1f8621b721ded521102dbb960426768b590adaee
Krzysztof Rutka <krzysztof.rutka@erlang-solutions.com>
2012-03-27 14:22:03 +0200

    Fix: Module name
    Issue #8333

--- apps/of_switch/src/of_switch_kernel.erl
+++ apps/of_switch/src/of_switch_kernel.erl
@@ -4,7 +4,7 @@
 %%% @doc Kernel module implementation of the OpenFlow Switch logic.
 %%% @end
 %%%-----------------------------------------------------------------------------
--module(of_swith_kernel).
+-module(of_switch_kernel).
 
 -behaviour(gen_switch).
 

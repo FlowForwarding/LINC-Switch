d4d407bca3eee1878fb4032e9c02f6dee97cdc0d
Krzysztof Rutka <krzysztof.rutka@erlang-solutions.com>
2012-04-05 09:02:55 +0200

    Get rid of an old version of of_netlink

--- apps/of_switch/src/of_switch_kernel.erl
+++
@@ -1,8 +0,0 @@
-%%%-----------------------------------------------------------------------------
-%%% @copyright (C) 2012, Erlang Solutions Ltd.
-%%% @doc Kernel module implementation of the OpenFlow Switch logic.
-%%% @end
-%%%-----------------------------------------------------------------------------
--module(of_switch_kernel).
-
--behaviour(gen_switch).

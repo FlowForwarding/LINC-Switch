c39b84860203ecbd4fd07c687753ce5070054486
Krzysztof Rutka <krzysztof.rutka@erlang-solutions.com>
2012-11-09 14:19:21 +0100

    Add template for instruction tests and some utils

--- apps/linc/test/linc_us3_instructions_tests.erl
+++ apps/linc/test/linc_us3_instructions_tests.erl
@@ -16,7 +16,9 @@
 
 %% @author Erlang Solutions Ltd. <openflow@erlang-solutions.com>
 %% @copyright 2012 FlowForwarding.org
--module(linc_us3_flow_tests).
+-module(linc_us3_instructions_tests).
+
+-import(linc_test_utils, [check_if_called/1]).
 
 -include_lib("eunit/include/eunit.hrl").
 -include_lib("of_protocol/include/of_protocol.hrl").
@@ -24,30 +26,51 @@
 
 %% Tests -----------------------------------------------------------------------
 
-flow_mod_test_() ->
+instruction_test_() ->
     {foreach,
      fun setup/0,
      fun teardown/1,
-     [{"Add simple flow", fun add_simple_flow/0},
-      {"Modify some flow", fun modify_flow/0}]}.
+     [{"Apply-Actions", fun apply_actions/0},
+      {"Clear-Actions", fun clear_actions/0},
+      {"Write-Actions", fun write_actions/0},
+      {"Write-Metadata", fun write_metadata/0},
+      {"Goto-Table", fun goto_table/0},
+      {"Empty instruction list", fun empty/0}]}.
 
-add_simple_flow() ->
-    %% FlowModAdd = #ofp_flow_mod{...
-    %% linc_us3_flow:modify(FlowModAdd),
-    %% Check if the flow was added correctly...
+apply_actions() ->
+    ApplyActionsInstruction = create_apply_actions(),
+    linc_us3_instructions:apply(pkt, [ApplyActionsInstruction]),
+    ?assert(check_if_called({linc_us3_actions, apply_list, 2})).
+
+clear_actions() ->
     ?assert(unimplemented).
 
-modify_flow() ->
+write_actions() ->
     ?assert(unimplemented).
+
+write_metadata() ->
+    ?assert(unimplemented).
+
+goto_table() ->
+    GotoInstruction = create_goto_table(5),
+    ?assertEqual({{goto, 5}, pkt},
+                 linc_us3_instructions:apply(pkt, [GotoInstruction])).
+
+empty() ->
+    ?assertEqual({stop, pkt}, linc_us3_instructions:apply(pkt, [])).
 
 %% Fixtures --------------------------------------------------------------------
 
 setup() ->
-    %% linc_us3_flow:create(),
     ok.
 
 teardown(ok) ->
-    %% linc_us3_flow:destroy(),
     ok.
 
 %% Helpers ---------------------------------------------------------------------
+
+create_apply_actions() ->
+    #ofp_instruction_apply_actions{actions = []}.
+
+create_goto_table(TableId) ->
+    #ofp_instruction_goto_table{table_id = TableId}.

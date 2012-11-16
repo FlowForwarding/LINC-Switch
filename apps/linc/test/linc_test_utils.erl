347d2982ed64376d977392f7e975f08016f55410
Konrad Kaplita <konrad.kaplita@erlang-solutions.com>
2012-11-16 09:24:24 +0100

    Refactored packet edit module responsible for Set-Field action and added tests.

--- apps/linc/test/linc_test_utils.erl
+++ apps/linc/test/linc_test_utils.erl
@@ -40,6 +40,11 @@ mock([port | Rest]) ->
                      end),
     mock(Rest);
 mock([group | Rest]) ->
+    ok = meck:new(linc_us3_groups),
+    ok = meck:expect(linc_us3_groups, apply,
+                     fun(_GroupId, _Pkt) ->
+                             ok
+                     end),
     mock(Rest);
 mock([instructions | Rest]) ->
     ok = meck:new(linc_us3_instructions),
@@ -66,7 +71,7 @@ unmock([port | Rest]) ->
     ok = meck:unload(linc_us3_port),
     unmock(Rest);
 unmock([group | Rest]) ->
-    ok = meck:unload(linc_us3_group),
+    ok = meck:unload(linc_us3_groups),
     unmock(Rest);
 unmock([instructions | Rest]) ->
     ok = meck:unload(linc_us3_instructions),

4e4417d6a0a0a9354e62d1824f38a8d171e449a8
sandhyan <sandhyan@sbcglobal.net>
2013-05-22 11:21:33 -0700

    Script to remove flow entries in table 0 and 1

---
+++ scripts/ryu/remove_flows_v1_3.py
@@ -0,0 +1,54 @@
+"""Deletes all flows using OpenFlow Protocol v1.3."""
+
+from ryu.base.app_manager import RyuApp
+from ryu.controller.ofp_event import EventOFPSwitchFeatures
+from ryu.controller.handler import set_ev_cls
+from ryu.controller.handler import CONFIG_DISPATCHER
+from ryu.controller.handler import MAIN_DISPATCHER
+from ryu.ofproto.ofproto_v1_2 import OFPG_ANY
+from ryu.ofproto.ofproto_v1_3 import OFP_VERSION
+from ryu.lib.mac import haddr_to_bin
+
+class L2Switch(RyuApp):
+    OFP_VERSIONS = [OFP_VERSION]
+
+    def __init__(self, *args, **kwargs):
+        super(L2Switch, self).__init__(*args, **kwargs)
+
+    @set_ev_cls(EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+
+    def switch_features_handler(self, ev):
+        """Handle switch features reply to remove flow entries in table 0 and 1."""
+        datapath = ev.msg.datapath
+        [self.remove_flows(datapath, n) for n in [0, 1]]
+    
+    def remove_flows(self, datapath, table_id):
+        """Removing all flow entries."""
+        parser = datapath.ofproto_parser
+        ofproto = datapath.ofproto
+        empty_match = parser.OFPMatch()
+        output = parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                        ofproto.OFPCML_NO_BUFFER)
+        write = parser.OFPInstructionActions(ofproto.OFPIT_WRITE_ACTIONS,
+                                             [output])
+        instructions = []
+        flow_mod = self.remove_table_flows(datapath, table_id,
+                                        empty_match, instructions)
+        print "deleting all flow entries in table ", table_id
+        datapath.send_msg(flow_mod)
+    
+
+    def remove_table_flows(self, datapath, table_id, match, instructions):
+        """Create OFP flow mod message to remove flows from table."""
+        ofproto = datapath.ofproto
+        flow_mod = datapath.ofproto_parser.OFPFlowMod(datapath, 0, 0, table_id,
+                                                      ofproto.OFPFC_DELETE, 0, 0,
+                                                      1,
+                                                      ofproto.OFPCML_NO_BUFFER,
+                                                      ofproto.OFPP_ANY,
+                                                      OFPG_ANY, 0,
+                                                      match, instructions)
+        return flow_mod
+
+
+

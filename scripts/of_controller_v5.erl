306068e558c7035e1721d16ea8ab6c5f0675ed12
Szymon Mentel <szymon.mentel@erlang-solutions.com>
2014-05-16 13:34:22 +0200

    Fix buffering packets in LINC when packet_in is sent (OFP 1.4)
    There has been an incorrect assumption that buffered packets are
    wrapped into a #linc_pkt. This resulted in a crash while matching
    on the packet.
    
    (Motivated by #322).

--- scripts/of_controller_v5.erl
+++ scripts/of_controller_v5.erl
@@ -698,6 +698,15 @@ scenario(bundle_error) ->
      %% Should still be empty
      flow_stats_request];
 
+%% Scenario motivated by #322
+%% (https://github.com/FlowForwarding/LINC-Switch/issues/322).
+%%
+%% This is a scenario for testing buffering packets in LINC between ofp_packet_in
+%% and ofp_packet_out messages. It sends a flow that generate a ofp_packet_in
+%% for every packet. The packets are buffered in the switch.
+scenario(forward_to_controller_and_buffer) ->
+    [flow_mod_output_to_port(1, controller, 128)];
+
 %% This scenario is empty as hello message is malformed and sent just after
 %% the connection is established.
 scenario(hello_with_bad_version) ->
@@ -1202,7 +1211,7 @@ flow_mod_output_to_port(InPort, OutPort, MaxLen) ->
                             name = in_port,
                             value = <<InPort:32>>},
     Match = #ofp_match{fields = [MatchField]},
-    Action = case InPort =:= controller of
+    Action = case OutPort =:= controller of
                  true ->
                      #ofp_action_output{port = OutPort, max_len = MaxLen};
                  false ->

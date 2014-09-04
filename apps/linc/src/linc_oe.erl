1b77ec549636988924341900fbc5968ac1eaaa89
Szymon Mentel <szymon.mentel@erlang-solutions.com>
2014-09-04 15:50:46 +0200

    Add utilities for storing/retrieving information on optical ports

--- apps/linc/src/linc_oe.erl
+++ apps/linc/src/linc_oe.erl
@@ -10,17 +10,43 @@ initialize() ->
     init_optical_links_ets(),
     init_ofp_to_optical_ports_map_ets().
 
-get_optical_peer_pid(SwitchID, PortNo) ->
-    throw(optical_peer_not_ready).
+get_optical_peer_pid(SwitchId, PortNo) ->
+    try ets:lookup_element(?OFP_TO_OPTICAL_PORT_ETS,
+                           {SwitchId, PortNo}, 2)
+    of
+        undefined ->
+            throw(optical_peer_not_ready);
+        Pid ->
+            Pid
+    catch
+        error:badarg ->
+            throw(optical_port_not_exists)
+    end.
 
-add_mapping_ofp_to_optical_port(SwitchID, PortNo, Pid) ->
-    ok.
+add_mapping_ofp_to_optical_port(SwitchId, PortNo, Pid)
+  when is_pid(Pid)->
+    case ets:update_element(?OFP_TO_OPTICAL_PORT_ETS,
+                            {SwitchId, PortNo}, {2, Pid}) of
+        true ->
+            ok;
+        false ->
+            throw(optical_port_not_exists)
+    end.
 
-is_port_optical(SwitchID, PortNo) ->
-    false.
+is_port_optical(SwitchId, PortNo) ->
+    Port = {SwitchId, PortNo},
+    [x] == [x || Pattern <- [{Port, '_'}, {'_', Port}],
+                 ets:match(?OPTICAL_LINKS_ETS, Pattern) /= []].
 
 init_optical_links_ets() ->
-    ok.
+    ets:new(?OPTICAL_LINKS_ETS, [named_table, public]),
+    {ok, Links} = application:get_env(linc, optical_links),
+    [ets:insert(?OPTICAL_LINKS_ETS, {OneEnd, OtherEnd})
+     || {OneEnd, OtherEnd} <- Links].
 
 init_ofp_to_optical_ports_map_ets() ->
-    ok.
+    {ok, Links} = application:get_env(linc, optical_links),
+    OFPPorts = lists:flatten([tuple_to_list(L) || L <- Links]),
+    ets:new(?OFP_TO_OPTICAL_PORT_ETS, [named_table, public]),
+    [ets:insert(?OFP_TO_OPTICAL_PORT_ETS, {P, _FuturePid = undefined})
+     || P <- OFPPorts].
---
+++ apps/linc/test/linc_oe_tests.erl
@@ -0,0 +1,96 @@
+-module(linc_oe_tests).
+
+-include_lib("eunit/include/eunit.hrl").
+
+%% The links configuration for the following topology. Numbers in |X|
+%% indicate switch id while numbers in {X} indicate ports. --- is for
+%% an optical link.
+%% ~~~||{1} |0| {2}||-----||{3}  |1|  {4}||-----||{5}  |2|  {6}||~~~
+%% NOTE: The ports need to be unique withing a switch as they are defined
+%% per capable switch.
+-define(OPTICAL_LINKS, [_Link = {{_SwID = 0, _PortNo = 2}, {1, 3}},
+                        {{1,4}, {2,6}}]).
+
+%% Generators ---------------------------------------------------------
+
+optical_utils_test_() ->
+    {foreach, fun() -> set_optical_links_config() end,
+     fun(_) -> ok end,
+     [fun optical_links_table_should_initialize_correctly/0,
+      fun adding_mapping_to_nonexistent_optical_port_should_fail/0,
+      fun getting_optical_peer_for_nonexistent_ofp_port_should_fail/0,
+      fun ofp_to_optical_port_table_should_initialize_correctly/0]}.
+
+%% Tests --------------------------------------------------------------
+
+optical_links_table_should_initialize_correctly() ->
+    %% GIVEN
+    OpticalPorts = get_optical_ports_from_config(),
+    APort = get_non_optical_port(),
+    
+    %% WHEN
+    linc_oe:initialize(),
+    
+    %% THEN
+    [?assert(linc_oe:is_port_optical(SId, PNo))
+     || {SId, PNo} <- OpticalPorts],
+    ?assertNot(linc_oe:is_port_optical(element(1, APort),
+                                       element(2, APort))).
+
+adding_mapping_to_nonexistent_optical_port_should_fail() ->
+    %% GIVEN
+    NonOptical = get_non_optical_port(),
+
+    %% WHEN
+    linc_oe:initialize(),
+
+    %% THEN
+    ?assertThrow(
+       optical_port_not_exists,
+       linc_oe:add_mapping_ofp_to_optical_port(element(1, NonOptical),
+                                               element(2, NonOptical),
+                                               self())).
+
+getting_optical_peer_for_nonexistent_ofp_port_should_fail() ->
+    %% GIVEN
+    NonOptical = get_non_optical_port(),
+
+    %% WHEN
+    linc_oe:initialize(),
+
+    %% THEN
+    ?assertThrow(optical_port_not_exists,
+                 linc_oe:get_optical_peer_pid(element(1, NonOptical),
+                                              element(2, NonOptical))).
+
+ofp_to_optical_port_table_should_initialize_correctly() ->
+    %% GIVEN
+    linc_oe:initialize(),
+    OpticalPorts = get_optical_ports_from_config(),
+    [NotReady | Ready] = OpticalPorts,
+
+    %% WHEN
+    [linc_oe:add_mapping_ofp_to_optical_port(SId, PNo, self())
+     || {SId, PNo} <- Ready],
+
+    %% THEN
+    [?assertEqual(self(), linc_oe:get_optical_peer_pid(SId, PNo))
+     || {SId, PNo} <- Ready],
+    ?assertThrow(optical_peer_not_ready,
+                 linc_oe:get_optical_peer_pid(element(1, NotReady),
+                                             element(2, NotReady))).
+
+%% Helpers ------------------------------------------------------------
+
+set_optical_links_config() ->
+    application:set_env(linc, optical_links, ?OPTICAL_LINKS).
+
+get_optical_ports_from_config() ->
+    lists:flatten(
+      [[OneEnd, OtherEnd]|| {OneEnd, OtherEnd} <- ?OPTICAL_LINKS]).
+
+get_non_optical_port() ->
+    Optical = get_optical_ports_from_config(),
+    APort = {0, 1},
+    ?assertNot(lists:member(APort, Optical)),
+    APort.
